34e22dab062499d920b067214c818790296f3d71
kyod741 <kyod741@staszic.waw.pl>
2024-11-21 16:59:57 +0100

    added using od EDITOR env variable alongside editor set i fcf config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use serde_json::Error as SerdeError;
+use std::collections::HashMap;
 use std::env;
 use std::format;
-use std::fs::create_dir_all;
 use std::fs;
-use std::path::Path;
+use std::fs::create_dir_all;
 use std::fs::OpenOptions;
-use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
-use serde_json::Error as SerdeError;
+use std::path::Path;
 use std::process::Command;
-use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(about=None, long_about=None)]
@@ -25,7 +25,7 @@ enum Commands {
         /// The key binded to the config file to edit
         key: String,
     },
-    /// Set the default editor
+    /// Set the default editor, you can also do it by setting enviroment variable EDITOR
     Editor {
         /// The name of the editor
         editor: String,
@@ -56,7 +56,7 @@ struct Config {
 
 impl Config {
     fn new() -> Config {
-        Config{
+        Config {
             editor: None,
             bindings: Some(HashMap::new()),
         }
@@ -64,30 +64,30 @@ impl Config {
 }
 const FSF_CONFIG_PATH: &str = "~/.config/fcf";
 
-fn expand_tilde(path: &str) -> String{
-    if path.starts_with("~"){
-        if let Some(home) = env::home_dir(){
+fn expand_tilde(path: &str) -> String {
+    if path.starts_with("~") {
+        if let Some(home) = env::home_dir() {
             return path.replacen("~", home.to_str().unwrap(), 1);
         }
     }
     return path.to_string();
 }
-fn touch(path: &str) -> (){
+fn touch(path: &str) -> () {
     let expanded_path = expand_tilde(path);
     let path = Path::new(&expanded_path);
 
-    if let Some(parent) = path.parent(){
+    if let Some(parent) = path.parent() {
         let _ = create_dir_all(parent);
     }
     let _ = OpenOptions::new().create(true).write(true).open(path);
-
 }
 
-fn parse_config() -> Config{
+fn parse_config() -> Config {
     touch(FSF_CONFIG_PATH);
 
-    let config_file = fs::read_to_string(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot read the config file");
-    
+    let config_file =
+        fs::read_to_string(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot read the config file");
+
     let parse: Result<Config, SerdeError> = serde_json::from_str(&config_file);
     let config: Config = match parse {
         Ok(content) => content,
@@ -97,19 +97,18 @@ fn parse_config() -> Config{
         }
     };
     config
-
 }
 
-fn write_config(config: &Config){
-    let file = fs::File::create(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot open the config file");
+fn write_config(config: &Config) {
+    let file =
+        fs::File::create(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot open the config file");
 
     if let Err(e) = serde_json::to_writer_pretty(file, &config) {
         println!("An error occurred while writing to the config file: {e}");
         return;
     }
-
 }
-fn set_default_editor(editors_name: &str) -> (){
+fn set_default_editor(editors_name: &str) -> () {
     let mut config: Config = parse_config();
 
     config.editor = Some(editors_name.to_string());
@@ -117,12 +116,13 @@ fn set_default_editor(editors_name: &str) -> (){
     println!("Successfully set default editor to {editors_name}");
 }
 
-fn bind(name: &str, file_path: &str){
+fn bind(name: &str, file_path: &str) {
     let mut config: Config = parse_config();
     if config.bindings.is_none() {
         config.bindings = Some(HashMap::new());
     }
-    config.bindings
+    config
+        .bindings
         .as_mut()
         .expect("This shouldn't have happened")
         .insert(name.to_string(), file_path.to_string());
@@ -130,46 +130,54 @@ fn bind(name: &str, file_path: &str){
     println!("Successfully binded {} with {file_path}", name);
 }
 
-fn remove_binding(name: &str){
+fn remove_binding(name: &str) {
     let mut config: Config = parse_config();
     if config.bindings.is_none() {
         config.bindings = Some(HashMap::new());
     }
-    match config.bindings
+    match config
+        .bindings
         .as_mut()
         .expect("Borrowing bindings HashMap failed")
-        .remove_entry(name){
-            None => {
-                println!("This binding does not exist");
-                return;
-            },
-            _ => (),
+        .remove_entry(name)
+    {
+        None => {
+            println!("This binding does not exist");
+            return;
+        }
+        _ => (),
     };
     write_config(&config);
     println!("Successfully removed {name} binding");
-
 }
 
-fn edit(name: &str) -> (){
+fn edit(name: &str) -> () {
+    let editor: String;
     let config: Config = parse_config();
-    let err = format!("The binding to {} does not exist", &name);
-    let _ = Command::new(config.editor
-            .expect("you have not configured your default editor"))
-        .arg(config.bindings
-            .expect("Failed to retrieve bindings")
-            .get(name)
-            .expect(&err))
+    let err = format!("The binding to {} does not exist", name);
+    if let Some(config_editor) = config.editor {
+        editor = config_editor;
+    } else {
+        editor = env::var("EDITOR").expect("You have not set editor in fcf config(fcf editor <editors_name>) nor EDITOR enviroment variable");
+    }
+    let _ = Command::new(editor)
+        .arg(
+            config
+                .bindings
+                .expect("Failed to retrieve bindings")
+                .get(name)
+                .expect(&err),
+        )
         .status();
-
-
 }
 
-fn print_config() -> (){
+fn print_config() -> () {
     touch(FSF_CONFIG_PATH);
-    let config_file = fs::read_to_string(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot read the config file");
+    let config_file =
+        fs::read_to_string(expand_tilde(FSF_CONFIG_PATH)).expect("Cannot read the config file");
     if config_file == "" {
         println!("The config is currently empty");
-    }else{
+    } else {
         println!("This is the config \n {config_file}");
     }
 }
@@ -178,20 +186,18 @@ fn main() {
     match &args.command {
         Commands::Edit { key } => {
             edit(&key);
-        },
+        }
         Commands::Editor { editor } => {
             set_default_editor(&editor);
-        },
+        }
         Commands::Bind { key, file } => {
             bind(&key, &file);
-        },
+        }
         Commands::RemoveBinding { key } => {
             remove_binding(&key);
-        },
+        }
         Commands::Print => {
             print_config();
         }
-
     }
 }
-
